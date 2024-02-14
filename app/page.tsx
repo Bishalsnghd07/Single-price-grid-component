70eb3e88d21838e3125f2b7001d1efb87e42f28f
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-02-14 16:44:02 +0530

    update pixel perfection

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ export default function Home() {
   return (
     <main className='flex flex-col min-h-screen p-8 justify-center items-center bg-neutral-Light-Gray overflow-hidden'>
     <div className='grid bg-white rounded-t-lg sm:w-[38.5rem] shadow-2xl'>
-      <article className='pr-9 pl-9 pb-8 pt-9 flex flex-col flex-wrap'>
+      <article className='pr-9 pl-9 pb-8 pt-8 flex flex-col flex-wrap'>
         <h1 className='  justify-start text-cyan-600 items-start font-semibold text-2xl'>Join our community</h1>
         <h2 className='text-primary-Bright-Yellow text-em font-semibold text-lg pt-[1rem] leading-7'>30-day hassle-free money back guarantee</h2>
-        <p className=' text-neutral-Grayish-Blue pt-3 text-base font-karla sm:max-w-[34.56rem] leading-6 pb-2'>Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
+        <p className=' text-neutral-Grayish-Blue pt-3 text-base font-karla sm:max-w-[34.56rem] leading-6 pb-1'>Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
       </article>
     </div>
     <div className="grid grid-cols-1 sm:grid-cols-2 sm:w-[38.5rem] rounded-b-lg  shadow-2xl">
@@ -19,13 +19,13 @@ export default function Home() {
           </div>
           <p className='pt-1 font-karla text-neutral-Light-Gray text-[0.99rem]'>Full access for less than $1 a day</p>
          
-        <a href='/' className='mt-8 bg-primary-Bright-Yellow w-full py-3 rounded-md shadow-xl text-center hover:bg-[#cdee12] text-base text-white cursor-pointer mb-8'>Sign up</a>        
+        <a href='/' className='mt-8 bg-primary-Bright-Yellow w-full py-3 rounded-md shadow-xl text-center hover:bg-[#cdee12] text-base text-white cursor-pointer mb-8 sm:mb-0'>Sign up</a>        
         </section>
       <section className='flex flex-col pt-8 pl-9 pb-2 pr-8 justify-start bg-[#41bbb9] rounded-b-lg sm:rounded-b-none sm:rounded-br-lg'>
         <h5 className='text-neutral-Light-Gray font-medium font-karla'>
         Why us
         </h5>
-        <p className='text-slate-300 font-karla pt-2 sm:max-w-[12.46rem] text-[0.99rem] pb-2'>Tutorials by industry experts Peer & expert code review Coding exercises Access to our GitHub repos Community forum Flashcard decks New videos every week</p>
+        <p className='text-slate-300 font-karla pt-2 sm:max-w-[12.46rem] text-[0.99rem] mb-2 sm:mb-0'>Tutorials by industry experts Peer & expert code review Coding exercises Access to our GitHub repos Community forum Flashcard decks New videos every week</p>
       </section>
     </div>
   </main>  
