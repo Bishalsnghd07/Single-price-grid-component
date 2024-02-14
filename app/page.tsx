15d0f1fa8f4e31b1ec634dd2592e948824fe67dd
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-02-14 16:16:50 +0530

    update pixel perfection

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
     <main className='flex flex-col min-h-screen p-8 justify-center items-center bg-neutral-Light-Gray overflow-hidden'>
     <div className='grid bg-white rounded-t-lg sm:w-[38.5rem] shadow-2xl'>
       <article className='pr-9 pl-9 pb-8 pt-9 flex flex-col flex-wrap'>
-        <h1 className=' text-primary-cyan justify-start items-start font-semibold text-2xl'>Join our community</h1>
-        <h2 className=' text-primary-Bright-Yellow font-semibold text-lg pt-[1rem]'>30-day hassle-free money back guarantee</h2>
-        <p className=' text-neutral-Grayish-Blue pt-3 text-base font-karla sm:max-w-[34.56rem]'>Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
+        <h1 className='  justify-start text-cyan-600 items-start font-semibold text-2xl'>Join our community</h1>
+        <h2 className='text-[#0fde0c] text-em font-semibold text-lg pt-[1rem] leading-7'>30-day hassle-free money back guarantee</h2>
+        <p className=' text-neutral-Grayish-Blue pt-3 text-base font-karla sm:max-w-[34.56rem] leading-6 pb-2'>Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
       </article>
     </div>
     <div className="grid grid-cols-1 sm:grid-cols-2 sm:w-[38.5rem] rounded-b-lg  shadow-2xl">
-        <section className='flex flex-col sm:items-start sm:justify-start bg-primary-cyan sm:rounded-bl-lg pt-8 pl-9 pr-8'>
+        <section className='flex flex-col sm:items-start sm:justify-start bg-primary-cyan sm:rounded-bl-lg pt-8 pl-9 pr-8 pb-2'>
           <h3 className='flex-wrap font-karla text-neutral-Light-Gray font-medium'>Monthly Subscription</h3>
           <div className="flex flex-row gap-2">
           <h4 className='text-3xl pt-4 font-semibold text-white'>$29
