5c2c85fe0cb9c3a405bca9cacbeb1d269660f95a
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-02-16 17:48:02 +0530

    pixel perfect update

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,30 @@
 
 export default function Home() {
   return (
-    <main className='flex flex-col min-h-screen p-4 justify-center items-center bg-neutral-Light-Gray overflow-hidden'>
-      <div className='grid bg-white rounded-t-lg sm:w-[38.5rem] shadow-2xl'>
-      <article className='pt-8 pr-8 pl-9 pb-9 flex flex-col flex-wrap'>
-        <h1 className='justify-start text-cyan-600 items-start font-semibold text-[1.60rem]'>Join our community</h1>
-        <h2 className='text-[#767601] text-em font-semibold text-base leading-7 pt-4'>30-day hassle-free money back guarantee</h2>
-        <p className='text-gray-500 pt-3 text-base font-karla sm:max-w-[34.56rem] leading-7 pb-1'>Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
-    </article>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 sm:w-[38.5rem] rounded-b-lg  shadow-2xl">
-      <section className='flex flex-col sm:items-start sm:justify-start bg-[#018576] sm:rounded-bl-lg pt-9 pl-9 pr-7 pb-8'>
-      <h3 className='flex-wrap font-karla text-lg text-white font-semibold'>Monthly Subscription</h3>
-      <div className="flex flex-row gap-2 pt-[0.9rem]">
-          <h4 className='text-3xl font-semibold text-white'>$29
-          </h4>
-           <p className='text-[#ffff] text-xs items-center pt-[0.6rem]'>per month</p>
-          </div>
-          <p className='font-karla text-white text-[0.99rem] pt-[0.5rem]'>Full access for less than $1 a day</p>
-         
-         <a href='/' className='mt-[1.6rem] bg-lime-700 w-full py-3 rounded-md shadow-xl text-center hover:bg-[#525c09] text-base text-white cursor-pointer'>Sign up</a>        
+    <main className="min-h-screen justify-center items-center flex flex-col overflow-hidden bg-neutral-Light-Gray">
+        <div className="grid sm:w-[38.5rem] rounded-t-lg shadow-lg">
+      <article className="pt-9 pl-8 pr-8 pb-6 bg-white">
+        <h1 className="text-cyan-600 text-2xl font-semibold pt-3">Join our community</h1>
+        <h2 className="text-base text-[#767601] font-semibold pt-[0.96rem]">30-day, hassle-free money back guarantee</h2>
+        <p className="pt-3 text-gray-500 font-karla text-base leading-6 pb-4">Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
+      </article>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 sm:w-[38.5rem]">
+      <section className="flex flex-col bg-[#018576] pt-10 pl-9 pr-7 pb-4 rounded-bl-lg">
+        <h2 className="text-white font-semibold text-lg">Monthly Subscription</h2>
+        <div className="flex flex-row pt-3 gap-3">
+            <h3 className="text-white font-semibold text-3xl">$29</h3>
+            <p className="text-white pt-[0.36rem]">per month</p>
+        </div>
+        <p className="text-white text-base pt-3">Full access for less than $1 a day</p>
+        <a href="/" className="mt-6 w-full bg-lime-700 hover:bg-[#525c09] py-3 text-center text-white font-semibold rounded-lg shadow-lg">Sign up</a>
       </section>
-      <section className='flex flex-col justify-start bg-[#008564] rounded-b-lg sm:rounded-b-none sm:rounded-br-lg pt-9 pl-9 pr-8 pb-8'>
-         <h5 className='flex-wrap font-karla text-lg text-white font-semibold'>
-         Why us
-         </h5>
-         <p className='text-white font-karla pt-4 sm:max-w-[13.76rem] text-[0.94rem] leading-6 mb-2 sm:mb-0'>Tutorials by industry experts Peer & expert code review Coding exercises Access to our GitHub repos Community forum Flashcard decks New videos every week</p>
-    </section>
-      </div>
-  </main>  
+
+      <section className="bg-[#008564] pt-9 pl-8 pr-8 pb-6 rounded-br-lg">
+        <h2 className="text-white font-semibold text-lg">Why us</h2>
+        <p className="text-white text-[0.94rem] font-karla pt-4 max-w-[12rem]">Tutorials by industry experts Peer & expert code review Coding exercises Access to our GitHub repos Community forum Flashcard decks New videos every week</p>
+      </section>
+        </div>
+    </main>
   );
 }
-
-    // <div className='grid bg-white rounded-t-lg sm:w-[38.5rem] shadow-2xl'>
-    //   <article className='pt-10 pr-8 pl-9 pb-9 flex flex-col flex-wrap'>
-    //     <h1 className='justify-start text-cyan-600 items-start font-semibold text-2xl pb-2'>Join our community</h1>
-    //     <h2 className='text-[#767601] text-em font-semibold text-lg leading-7'>30-day hassle-free money back guarantee</h2>
-    //     <p className=' text-gray-500 pt-3 text-base font-karla sm:max-w-[34.56rem] leading-6 pb-4'>Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
-    //   </article>
-    // </div>
-    // <div className="grid grid-cols-1 sm:grid-cols-2 sm:w-[38.5rem] rounded-b-lg  shadow-2xl">
-    //     <section className='flex flex-col sm:items-start sm:justify-start bg-[#018576] sm:rounded-bl-lg pt-14 pl-9 pr-7 pb-2'>
-    //       <h3 className='flex-wrap font-karla text-lg text-[#ffffff] font-semibold'>Monthly Subscription</h3>
-    //       <div className="flex flex-row gap-2">
-    //       <h4 className='text-3xl pt-4 font-semibold text-white'>$29
-    //       </h4>
-    //       <p className='text-[#ffff] text-xs items-center pt-[1.7rem]'>per month</p>
-    //       </div>
-    //       <p className='pt-1 font-karla text-white text-[0.99rem]'>Full access for less than $1 a day</p>
-         
-    //     <a href='/' className='mt-7 bg-lime-700 w-full py-3 rounded-md shadow-xl text-center hover:bg-[#525c09] text-base text-white cursor-pointer mb-4 sm:mb-4'>Sign up</a>        
-    //     </section>
-    //   <section className='flex flex-col p-9 justify-start bg-[#008564] rounded-b-lg sm:rounded-b-none sm:rounded-br-lg'>
-    //     <h5 className='text-white font-semibold font-karla'>
-    //     Why us
-    //     </h5>
-    //     <p className='text-white font-karla pt-4 sm:max-w-[13.76rem] text-[0.99rem] mb-2 sm:mb-0'>Tutorials by industry experts Peer & expert code review Coding exercises Access to our GitHub repos Community forum Flashcard decks New videos every week</p>
-    //   </section>
-    // </div>
